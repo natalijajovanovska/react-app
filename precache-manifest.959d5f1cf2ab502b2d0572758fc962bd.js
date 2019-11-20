19c3e01851bf243aa79a1a2652545c79341e6480
Natalija Jovanovska <natalija.j@live.com>
2019-11-20 20:33:54 +0100

    Updates

---
+++ precache-manifest.959d5f1cf2ab502b2d0572758fc962bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "377ce6343d60ddb17c2bee62a0561a87",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "1d599f2d6e0ca50c30c8",
+    "url": "/react-app/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "c49481b080e240d5b725",
+    "url": "/react-app/static/css/main.cd3fb384.chunk.css"
+  },
+  {
+    "revision": "1d599f2d6e0ca50c30c8",
+    "url": "/react-app/static/js/2.19c5e48f.chunk.js"
+  },
+  {
+    "revision": "c49481b080e240d5b725",
+    "url": "/react-app/static/js/main.c8b91b5e.chunk.js"
+  },
+  {
+    "revision": "e55ecc9cdff2ebd88279",
+    "url": "/react-app/static/js/runtime-main.b56ced4c.js"
+  },
+  {
+    "revision": "833c5a26fd8ea58135557ad909f0608f",
+    "url": "/react-app/static/media/logo193.833c5a26.png"
+  }
+]);
